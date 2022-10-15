de724df4429bb33be77f04137bebac70c1d0343c
Luis Diego Hernández Mora <luisdiher22@gmail.com>
2022-10-14 19:11:20 -0600

    Proyecto Inicial 14/10/2022
    Proyecto vacío

---
+++ ProyectoI/src/proyectoi/ProyectoI.java
@@ -0,0 +1,15 @@
+
+package proyectoi;
+
+/**
+ *
+ * @author Kendall, Melissa y Luis Diego
+ */
+public class ProyectoI {
+
+  
+    public static void main(String[] args) {
+       
+    }
+    
+}
